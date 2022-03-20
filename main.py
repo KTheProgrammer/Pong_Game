aa6c139c026b216a12832cab46acfe347780383b
Kevio McClellan <57043715+KTheProgrammer@users.noreply.github.com>
2022-03-20 12:52:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Score
+import time
 
 screen = Screen()
 line = Turtle()
-score1 = Turtle()
-score2 = Turtle()
+score_L = Score((-100, 200))
+score_R = Score((100, 200))
+paddle_L = Paddle((-470, 0))
+paddle_R = Paddle((470, 0))
+ball = Ball()
 
 screen.setup(width=1000, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
-
-score1.speed("fastest")
-score1.hideturtle()
-score1.penup()
-score1.color("white")
-score1.goto(x=-100, y=200)
-score1.write("0", align="center", font=("Courier", 80, "normal"))
-
-score2.speed("fastest")
-score2.hideturtle()
-score2.penup()
-score2.color("white")
-score2.goto(x=100, y=200)
-score2.write("0", align="center", font=("Courier", 80, "normal"))
+screen.tracer(0)
 
 line.hideturtle()
 line.shape("square")
@@ -38,53 +32,20 @@ for num in range(15):
     line.pendown()
 
 
-def p1up():
-    paddle1.setheading(90)
-    paddle1.forward(20)
-
-
-def p1down():
-    paddle1.setheading(270)
-    paddle1.forward(20)
-
-
-def p2up():
-    paddle2.setheading(90)
-    paddle2.forward(20)
-
-
-def p2down():
-    paddle2.setheading(270)
-    paddle2.forward(20)
-
-
 screen.listen()
-screen.onkey(fun=p1up, key="w")
-screen.onkey(fun=p1down, key="s")
-screen.onkey(fun=p2up, key="Up")
-screen.onkey(fun=p2down, key="Down")
+screen.onkey(fun=paddle_L.p1up, key="w")
+screen.onkey(fun=paddle_L.p1down, key="s")
+screen.onkey(fun=paddle_R.p1up, key="Up")
+screen.onkey(fun=paddle_R.p1down, key="Down")
 
+still_playing = True
+while still_playing:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
 
-paddle1 = Turtle(shape="square")
-paddle1.speed("fastest")
-paddle1.penup()
-paddle1.right(90)
-paddle1.color("white")
-paddle1.goto(-470, 50)
-paddle1.shapesize(stretch_len=5, stretch_wid=1)
-
-
-paddle2 = Turtle(shape="square")
-paddle2.speed("fastest")
-paddle2.penup()
-paddle2.right(90)
-paddle2.color("white")
-paddle2.goto(470, 50)
-paddle2.shapesize(stretch_len=5, stretch_wid=1)
-
-ball = Turtle(shape="square")
-ball.color("white")
-ball.goto(x=0, y=0)
-
+    # Detect collision with wall
+    if ball.ycor() > 250 or ball.ycor() < -250:
+        ball.bounce_down()
 
 screen.exitonclick()
